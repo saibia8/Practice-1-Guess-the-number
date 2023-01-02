8ab8954a71889b6d7ff4039ff600edc94a022cbe
saibia8 <saibia.design@gmail.com>
2023-01-02 18:20:42 +0100

    Almost done

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+// console.log(document.querySelector('.guess-message').textContent);
+
+// document.querySelector('.number-input').value = 13;
+
+// const eventHandler = function () {
+//    console.log(document.querySelector('.number-input').value)
+// }
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  
+
+  // Empty input
+  if (!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Input some number!';
+  }
+
+  // Player won
+  else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'You won!😎';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.question').style.width = '50rem';
+    document.querySelector('.question').textContent = secretNumber;
+  }
+
+  // The number is too high
+  else if (guessingNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess-message').textContent = 'Game over!🥲';
+    }
+  }
+
+  // The number is too low
+  else if (guessingNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.guess-message').textContent = 'Game over!🥲';
+    }
+  }
+});
